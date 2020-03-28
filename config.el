942b5981f1b6298fd9b83d00aacdbc936ce670a3
Phil Piwonka <philpiwonka@gmail.com>
2020-03-28 16:24:45 -0700

    DOOM EMACS piwonka start

---
+++ config.el
@@ -0,0 +1,152 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Phil Piwonka"
+      user-mail-address "philpiwonka@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "JetBrains Mono" :size 14))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+;(setq doom-theme 'doom-laserwave)
+(setq doom-theme 'doom-one)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/ws/orgmode/src/PiwonkaOrgMode/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
+
+;; Make it so I can pull up M-x more easily
+(global-set-key "\C-x\C-m" 'counsel-M-x)
+(global-set-key "\C-xm" 'counsel-M-x)
+
+;; Make jumping around lists/parens easy
+(global-set-key "\M-n" 'forward-list)
+(global-set-key "\M-p" 'backward-list)
+
+;; Display continuous lines
+;; (setq-default truncate-lines nil)
+;; truncate even even when screen is split into multiple windows
+;; (setq-default truncate-partial-width-windows nil)
+
+;; Various Preferences
+(setq kill-whole-line t)
+
+;; Aliases
+;;
+(defalias 'qrr 'query-replace-regexp)
+(defalias 'edb 'ediff-buffers)
+(defalias 'afm 'auto-fill-mode)
+(defalias 'gits 'magit-status)
+(defalias 'kr 'helm-show-kill-ring)
+
+(setq confirm-kill-emacs 'y-or-n-p)
+
+;;; ORG
+(use-package! org
+  :config
+  (progn
+    (global-set-key "\C-c\C-r" 'org-refile)
+    (global-set-key "\C-cr" 'org-refile)
+    (setq wnka/org-path "~/ws/orgmode/src/PiwonkaOrgMode/")
+    (setq wnka/org-notes-path "~/ws/orgmode/src/PiwonkaOrgMode/notes/")
+    (setq org-agenda-files (mapcar #'(lambda (orgfile) (concat wnka/org-path orgfile))
+                                   (list
+                                    "inbox.org"
+                                    "work.org"
+                                    "web.org"
+                                    )))
+
+    (setq org-agenda-custom-commands
+          '(("n" todo "NEXT") ; Todo items that are marked "NEXT"
+            ))
+
+    (setq org-agenda-sorting-strategy '(time-up priority-down category-up))
+    (setq org-agenda-skip-deadline-if-done t)
+    (setq org-agenda-skip-scheduled-if-done t)
+    (setq org-agenda-window-setup 'current-window)
+    (setq org-deadline-warning-days 0)
+
+    (setq org-use-property-inheritance t)
+
+    (setq org-export-section-number-format '((("1" "."))
+                                             . " - "))
+
+    (setq org-capture-templates
+          '(
+            ("t" "Todo" entry (file+headline (lambda () (concat wnka/org-path "inbox.org")) "Inbox")
+             "* TODO %?\n  %i\n")
+            ("d" "Todo Today" entry (file+headline (lambda () (concat wnka/org-path "inbox.org")) "Inbox")
+             "* TODO %?\n  SCHEDULED: %t")
+            ("c" "Todo with Clipboard" entry (file+headline (lambda () (concat wnka/org-path "inbox.org")) "Inbox")
+             "* TODO %?\n  %c" :empty-lines 1)
+            )
+          )
+
+    (setq org-log-done 'time)
+    (setq org-refile-use-outline-path nil)
+    (setq org-agenda-start-on-weekday nil)
+    (setq org-refile-targets '(
+                               ("work.org" . (:level . 1))
+                               ))
+    (setq org-todo-keywords
+          '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)" "CANCELLED(c)")))
+    (setq org-use-fast-todo-selection t)
+    )
+  )
+
+;;; ORG-JOURNAL config
+(use-package! org-journal
+  :config
+  (progn
+    (setq org-journal-dir "~/ws/orgmode/src/PiwonkaOrgMode/journal/")
+    ;;; New journal entry on both C-j and j so I can be sloppy
+    (global-set-key "\C-c\C-j" 'org-journal-new-entry)
+    (global-set-key "\C-cj" 'org-journal-new-entry)
+    ))
+
+;;; ORG-SUPER-AGENDA config
+(use-package! org-super-agenda
+  :config
+  (progn
+    (setq org-super-agenda-groups
+          '((:auto-outline-path t)))
+    (org-super-agenda-mode)
+    )
+  )
